cbe2a9d98a33e1dc1093c74d099890bb5c93950a
joey soroka <joeysoroka@outlook.com>
2022-02-21 09:07:55 -0800

    2/21/22 - Joey

---
+++ StemProject/Structs.h
@@ -0,0 +1,59 @@
+#include <vector>
+using namespace std;
+
+struct states
+{
+	vector <vector<int>> s // state by state values of land costs by acre, square ft cost of parking structure, sqare ft cost of  
+	{
+		{12356, 88, }, // Alabama
+		{10000, 120, }, // Alaska
+		{4328, 90, }, // Arizona
+		{6739, 88, }, // Arkansas
+		{39092, 120, }, // California
+		{6462, 94, }, // Colorado
+		{128824, 119, }, // Connecticut
+		{57692, 108, }, // Delaware
+		{28961, 88, }, // Florida
+		{14242, 92, }, // Georgia
+		{NA, 133, }, // Hawaii
+		{3435, 70, }, // Idaho
+		{23492, 125, }, // Illinois
+		{16903, 95, }, // Indiana
+		{6590, 70, }, // Iowa
+		{4220, 70, }, // Kansas
+		{7209, 91, }, // Kentucky
+		{12908, 91, }, // Louisiana
+		{6142, 95, }, // Maine
+		{75429, 108, }, // Maryland
+		{102214, 119, }, // Massachesetss
+		{23765, 110, }, // Michigan
+		{8191, 91, }, // Minnesota
+		{5565, 95, }, // Mississippi
+		{7233, 80, }, // Missouri
+		{2283, 70, }, // Montana
+		{2936, 70, }, // Nebraska
+		{2116, 110, }, // Nevada
+		{19840, 95, }, // New Hampshire
+		{196410, 140, }, // New Jersey 
+		{1931, 70, }, // New Mexico
+		{41314, 140, }, // New York
+		{16230, 88, }, // North Carolina
+		{2517, 70, }, // North Dakota
+		{32077, 90, }, // Ohio
+		{7364, 90, }, // Oklahoma
+		{6503, 108, }, // Oregon
+		{31923, 120, },	// Pennsylvania
+		{133730, 130, }, // Rhode Island
+		{17610, 92, }, // South Carolina
+		{2135, 70, }, // South Dakota
+		{14411, 91, }, // Tennessee
+		{7542, 90, }, // Texas
+		{4664, 70, }, // Utah
+		{7439, 95, }, // Vermont
+		{21921, 92, },	// Virginia
+		{16752, 117, }, // Washington
+		{10537, 80, }, // West Virginia
+		{9924, 80, }, // Wisconsin
+		{1558, 70, }, // Wyoming
+	};
+};
