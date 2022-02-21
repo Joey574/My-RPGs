8ffd649db67002c78c9fd633693f5d3e440674b9
joey soroka <joeysoroka@outlook.com>
2022-02-21 09:08:41 -0800

    2/21/22 - Joey

--- StemProject/StemProject.cpp
+++ StemProject/StemProject.cpp
@@ -4,6 +4,7 @@ File Name: StemProject.cpp
 Pseudocode:
 Maintenance Log: 
 2/14/21: Started Project
+2/21/22: Added 2d vector for state info, included info about cost per acre and parking structure cost
 */
 
 #include <iostream>
